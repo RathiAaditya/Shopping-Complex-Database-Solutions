54f0bd6ef7fd8711c3ea2f537848f4d21ccab12b
RathiAaditya <f20202191@hyderabad.bit-pilani.ac.in>
2022-04-16 17:13:22 +0530

    Testing

--- mall/models.py
+++ mall/models.py
@@ -1,3 +1,12 @@
+from xmlrpc.client import boolean
 from django.db import models
 
 # Create your models here.
+
+
+class Customer:
+    mobile_no: int
+    firstname: str
+    lastname: str
+    Vaccination_Status: boolean
+    Vehicle_id: str
