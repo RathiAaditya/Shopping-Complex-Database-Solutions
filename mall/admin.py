d5139a9a0aefd6793f8d07cada3dc2532483ca67
Aaditya  Rathi <93321989+RathiAaditya@users.noreply.github.com>
2022-04-18 11:52:03 +0530

    Update admin.py

--- mall/admin.py
+++ mall/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Booking, Bound_by, Companies, Company_contact_no, Contracts, Customer, Invoice, Provides, Services, Shops, Slots
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Companies)
+admin.site.register(Company_contact_no)
+admin.site.register(Invoice)
+admin.site.register(Contracts)
+admin.site.register(Shops)
+admin.site.register(Slots)
+admin.site.register(Provides)
+admin.site.register(Services)
+admin.site.register(Bound_by)
