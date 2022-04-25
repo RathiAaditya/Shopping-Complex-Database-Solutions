c858ec1c9ffec6869afee9a71086fbfb232fd2aa
Shreyas Dixit <shreyasdixit62442@gmail.com>
2022-04-25 21:47:16 +0530

    removed-unnecessary-files

--- mall/urls.py
+++ mall/urls.py
@@ -3,10 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add', views.add, name='add'),
-    path('login', views.login, name='login'),
     path('customer',views.customerdata,name='customer'),
     path('company',views.companydata,name='company'),
     path('invoice',views.invoicedata,name='invoice'),
-    path('company/search',views.search,name='search')
+    path('company/search',views.searchcompany,name='companysearch'),
+    path('customer/search',views.searchcustomer,name='customersearch')
 ]
