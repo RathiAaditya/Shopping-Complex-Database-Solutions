6c316193d2830d05559fa63939709d1618d0960c
Sankalp Kulkarni <93430622+castform151@users.noreply.github.com>
2022-04-27 19:50:01 +0530

    new html froms

--- shmalldb/settings.py
+++ shmalldb/settings.py
@@ -81,12 +81,11 @@ WSGI_APPLICATION = 'shmalldb.wsgi.application'
 DATABASES = {
 
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mall2',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': 'shreyas123',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shoppingmall2',
+        'USER': 'postgres',
+        'PASSWORD': 'sankalp02',
+        'HOST': 'localhost',
     }
 
 }
